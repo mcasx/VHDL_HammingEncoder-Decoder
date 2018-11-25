aebcb614ec14f5309fded5b44471350cec0c47b3
Manuel Xarez <manuelxarez@ua.pt>
2018-11-25 10:02:57 +0000

    Changed adder to RippleCounter to save hardware

--- SerializedDecoder/ControlUnit.vhd
+++ SerializedDecoder/ControlUnit.vhd
@@ -13,45 +13,9 @@ ENTITY ControlUnit IS
 END ControlUnit;
 
 ARCHITECTURE behavior OF ControlUnit IS
-	signal s1, q1: std_logic_vector(3 downto 0) := "1111";
 	signal s_reset: std_logic;
+	signal q1: std_logic_vector(3 downto 0);
 BEGIN
-	flipFlop1: storage.flipFlopDSimul
-	port map(
-		clk => clk,
-		D => s1(0),
-		nSet => '1',
-		nRst => '1',
-		Q => q1(0)
-	);
-	
-	flipFlop2: storage.flipFlopDSimul
-	port map(
-		clk => clk,
-		D => s1(1),
-		nSet => '1',
-		nRst => '1',
-		Q => q1(1)
-	);
-	
-	flipFlop3: storage.flipFlopDSimul
-	port map(
-		clk => clk,
-		D => s1(2),
-		nSet => '1',
-		nRst => '1',
-		Q => q1(2)
-	);
-	
-	flipFlop4: storage.flipFlopDSimul
-	port map(
-		clk => clk,
-		D => s1(3),
-		nSet => '1',
-		nRst => '1',
-		Q => q1(3)
-	);
-	
 	memory: decoder.ROM
 	port map(
 		index => q1,
@@ -61,10 +25,11 @@ BEGIN
 		line4 => line4
 	);
 	
-	adder: decoder.OneAdder
+	counter: decoder.RippleCounter
 	port map(
-		a => q1,
-		x => s1
+		clk => clk,
+		x => q1,
+		reset => '1'
 	);
 	
 	ready <= not q1(0) and q1(1) and q1(2) and q1(3);
---
+++ SerializedDecoder/RippleCounter.vhd
@@ -0,0 +1,59 @@
+LIBRARY decoder;
+
+LIBRARY storage;
+LIBRARY ieee;
+
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY RippleCounter IS
+    PORT (
+		  clk: in std_logic;
+        x: out std_logic_vector(3 downto 0);
+		  reset: in std_logic
+    );
+END RippleCounter;
+
+ARCHITECTURE logicFunction OF RippleCounter IS 
+	signal nq1, nq2, nq3, nq4: std_logic;
+BEGIN
+	flipFlopDSimul1: storage.flipFlopDSimul
+	port map(
+		clk => clk,
+		D => nq1,
+		nSet => '1',
+		nRst => reset,
+		Q => x(0),
+		nQ => nq1
+	);
+	
+	flipFlopDSimul2: storage.flipFlopDSimul
+	port map(
+		clk => nq1,
+		D => nq2,
+		nSet => '1',
+		nRst => reset,
+		Q => x(1),
+		nQ => nq2
+	);
+	
+	flipFlopDSimul3: storage.flipFlopDSimul
+	port map(
+		clk => nq2,
+		D => nq3,
+		nSet => '1',
+		nRst => reset,
+		Q => x(2),
+		nQ => nq3
+	);
+	
+	flipFlopDSimul4: storage.flipFlopDSimul
+	port map(
+		clk => nq3,
+		D => nq4,
+		nSet => '1',
+		nRst => reset,
+		Q => x(3),
+		nQ => nq4
+	);
+END logicFunction;
